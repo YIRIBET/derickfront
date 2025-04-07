2cfa2c82c10e78addabf86384836b092ed465ee5
YismaelDiazH <105748001+YismaelDiazH@users.noreply.github.com>
2025-04-07 06:26:16 -0600

    Cambio de colores y logo de empresa

--- src/Components/admin/Navbar.jsx
+++ src/Components/admin/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import logo from "../../assets/logop.jpg";
 export default function Navbar({onOpen}) {
   return (
     <>
       <div className="navbar bg-base-100 shadow-sm">
         <div className="navbar-start">
          
-          <a className="btn btn-ghost text-xl">Restaurant</a>
+         
+          {/* anadir imgen de logo */}
+            <img src={logo} alt="Logo" className="w-12 h-12 rounded-full" />
+           
+
         </div>
         <div className="navbar-center ">
         <input type="text" placeholder="Search" className="input input-bordered w-48 md:w-auto" />
