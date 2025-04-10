ff22f5818e9ef246d6242c5397970409e496ed50
MartinOM321753 <133397788+MartinOM321753@users.noreply.github.com>
2025-04-10 15:46:39 -0600

    Implementacion de Roles en el appRouter

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,6 +4,7 @@ import {
   createRoutesFromElements,
   Route,
   RouterProvider,
+  Navigate,
 } from 'react-router-dom';
 
 import Home from '../pages/Cliente/Home';
@@ -11,50 +12,81 @@ import Menu from '../pages/Cliente/Menu';
 import Orders from '../pages/Cliente/Orders';
 import Cart from '../Components/Cart';
 import Profile from '../pages/Cliente/Profile';
-import Login from '../Components/Login'
+import Login from '../Components/Login';
 import Register from '../Components/Register';
 import Food from '../pages/Cliente/Food';
 import Dashboard from '../pages/Restaurantero/Dashbord';
-import AuthContext from '../config/context/auth-context'; // El contexto de autenticación
-import NotFound from '../pages/errorPages/404'; // Tu componente de 404
-import AdminLayout from './../layout/adminLayout'
-import RequestReset from '../Components/RequestReset'
-import ResetPassword from '../Components/ResetPassword'
-
+import AuthContext from '../config/context/auth-context';
+import NotFound from '../pages/errorPages/404';
+import AdminLayout from './../layout/adminLayout';
+import RequestReset from '../Components/RequestReset';
+import ResetPassword from '../Components/ResetPassword';
 
 const AppRouter = () => {
-  const { user } = useContext(AuthContext) || {}; // Asegurarnos de que user esté definido
-  const isUserSignedIn = user?.signed || false; // Usar optional chaining y asignar un valor predeterminado si no está definido
+  const { user } = useContext(AuthContext);
+  const isUserSignedIn = user?.signed;
+
+  const routesFromRole = (role) => {
+    switch (role) {
+      case 'ADMIN_ROLE':
+        return (
+          <Route path="/" element={<AdminLayout />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="Menu/:restaurantId" element={<Menu />} />
+            <Route path="food/:menuId" element={<Food />} />
+            <Route path="order" element={<Orders />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="perfil" element={<Profile />} />
+            <Route path="request-password" element={<RequestReset />} />
+            <Route path="reset-password" element={<ResetPassword />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Route>
+        );
+
+      case 'CLIENT_ROLE':
+        return (
+          <Route path="/" element={<AdminLayout />}>
+            <Route path="Menu/:restaurantId" element={<Menu />} />
+            <Route path="food/:menuId" element={<Food />} />
+            <Route path="order" element={<Orders />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="perfil" element={<Profile />} />
+            <Route path="request-password" element={<RequestReset />} />
+            <Route path="reset-password" element={<ResetPassword />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Route>
+        );
+
+      case 'RESTAURANTERO':
+        return (
+          <Route path="/" element={<AdminLayout />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="Menu/:restaurantId" element={<Menu />} />
+            <Route path="food/:menuId" element={<Food />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Route>
+        );
+
+      default:
+        return <Route path="*" element={<NotFound />} />;
+    }
+  };
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-        {/* Si el usuario está autenticado */}
         {isUserSignedIn ? (
-          <Route path="/" element={<AdminLayout />}>
-            {/* Rutas del administrador */}
-            <Route path="/Menu/:restaurantId" element={<Menu />} />
-            <Route path="/food/:menuId" element={<Food />} />
-            <Route path="/order" element={<Orders />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/perfil" element={<Profile />} />
-            <Route path="/request-password" element={<RequestReset />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Route>
+          routesFromRole(user?.role) // O user?.roles[0]?.rol si manejas array
         ) : (
-          // Si el usuario no está autenticado, redirigir a la página de login
           <>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/request-password" element={<RequestReset />} />
             <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/*" element={<NotFound />} />
           </>
         )}
-
-        {/* Ruta por defecto para cualquier ruta no encontrada */}
-        <Route path="/*" element={<NotFound />} />
       </>
     )
   );
